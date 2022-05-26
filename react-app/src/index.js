696983208928f37f2db12706901106057655f9a0
Goat.St3 <stefano2301@live.it>
2022-05-26 15:26:43 +0200

    es05

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Hello from './Hello';
 import HelloName from './HelloName';
+import Sum from './sum';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Hello/>
     <HelloName/>
+    <Sum/>
   </React.StrictMode>
 );
---
+++ react-app/src/sum.js
@@ -0,0 +1,4 @@
+export default function Sum(){
+    const foo =(a,b)=><h2>{a+b}</h2>
+    return foo (4,5)
+}
