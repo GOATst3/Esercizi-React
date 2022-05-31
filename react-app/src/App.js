5bba9221eefa9ba691e9d084278b3d96534c5d15
Goat.St3 <stefano2301@live.it>
2022-05-31 14:40:30 +0200

    es14

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import {Hello} from './Hello.js'
+import { Message } from "./Message.js";
 export class App extends Component{
     render(){
         return(
             <div>
                 <Hello/>
-                <Hello/>
+                <Message/>
             </div>
         )
     }
