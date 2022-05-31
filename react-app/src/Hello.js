c8bf33e9d32b563a9f85e77ef5a8c66851484730
Goat.St3 <stefano2301@live.it>
2022-05-31 14:26:30 +0200

    es11

--- react-app/src/Hello.js
+++ react-app/src/Hello.js
@@ -1,6 +1,11 @@
 import React from "react"
 export class Hello extends React.Component{
     render(){
-        return <h1>Hello, world</h1>
+        return (
+            <>
+            <h1>Hello, world</h1>
+            <p>What a beautiful day!</p>
+            </>
+        )
     }
 }
