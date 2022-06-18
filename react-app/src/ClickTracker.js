98d1997bd000dc9bf5da087681ab535b9cf5e9de
Goat.St3 <stefano2301@live.it>
2022-06-18 13:43:02 +0200

    es36 done

--- react-app/src/ClickTracker.js
+++ react-app/src/ClickTracker.js
@@ -4,9 +4,30 @@ export default class ClickTracker extends React.Component{
     render(){
         return (
             <>
-            <button name="First" onClick={this.props.childClick}>Button 1</button>
-            <button name="Second" onClick={this.props.childClick}>Button 3</button>
-            <button name="Third" onClick={this.props.childClick}>Button 2</button>
+                <button name="First" onClick={this.props.childClick}>
+                    <img 
+                    src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Decorative-Numbers/Number_One_Neon_Transparent_PNG_Image.png?m=1521603647"
+                     onClick={this.props.childClick}
+                     name="First"
+                     height={'40px'} width={'40px'}
+                    />
+                </button>
+                <button name="Second" onClick={this.props.childClick}>
+                    <img
+                     src='https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Decorative-Numbers/Number_Two_Neon_Transparent_PNG_Image.png?m=1521603646'
+                     onClick={this.props.childClick}
+                     name="Second"
+                     height={'40px'} width={'40px'}
+                    />
+                </button>
+                <button name="Third" onClick={this.props.childClick}>
+                    <img 
+                     src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Decorative-Numbers/Number_Three_Neon_Transparent_PNG_Image.png?m=1521603645" 
+                     onClick={this.props.childClick} 
+                     name="Third"
+                     height={'40px'} width={'40px'}
+                    />
+                </button>
             </>
         )
     }
