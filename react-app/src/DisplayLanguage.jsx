d6ad8398c5d70ede48202d36d80a4046efc1386e
Goat.St3 <stefano2301@live.it>
2022-06-28 18:28:32 +0200

    es59 done

--- react-app/src/DisplayLanguage.jsx
+++ react-app/src/DisplayLanguage.jsx
@@ -4,7 +4,9 @@ export default class DisplayLanguage extends React.Component{
     render(){
         return(
             <LanguageContext.Consumer>
-                
+                {language=>{
+                return<h1>{language}</h1>
+                }}
             </LanguageContext.Consumer>
         )
     }
