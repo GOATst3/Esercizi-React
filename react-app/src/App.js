5e1e916cabec8d847655c48e4c22f4382a1b966b
Goat.St3 <stefano2301@live.it>
2022-06-28 11:45:39 +0200

    es54

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,13 @@
+import React from "react"
+import Container from './Container';
+import Child from './Child';
+
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        <Child/>
+      </Container>
     </div>
   );
 }
