f304cb992f81dec99d966c7729b4b1de1aac2746
Goat.St3 <stefano2301@live.it>
2022-07-03 15:37:20 +0200

    es67 done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,13 @@
 import Counter from "./Counter";
+import { useState } from "react";
 function App() {
+  const [toggle,setToggle]=useState(true)
+  const toggleHandler=()=>setToggle((s)=>!s)
   return (
     <div className="App">
-      <Counter onCounterChange = {(count) =>console.log('the counter is '+count)}/>
+      {toggle&&<Counter onCounterChange = {(count) =>console.log('the counter is '+count)}/>}
+      {toggle&&<button onClick={toggleHandler}>unmount</button>}
+      {!toggle&&<button onClick={toggleHandler}>mount</button>}
     </div>
   );
 }
--- react-app/src/Counter.js
+++ react-app/src/Counter.js
@@ -1,14 +1,27 @@
 import {useState,useEffect} from 'react'
 export default function Counter (props){
     const [count, setCount] = useState(0)
+
+    function interval(){
+        setInterval(() => {
+        setCount((c)=>c+1)
+        }, 1000);
+    }
+
+
+    useEffect(()=>{
+        interval()
+        return ()=> {
+            console.log('unmounting...')
+            clearInterval(interval)
+            console.log('unmounted')
+        }
+    },[])
     useEffect(()=>props.onCounterChange(count),[count])
-    
-    const clickHandler = () =>setCount(c=>c+1)
 
     return (
         <>
-            <h1>{count}</h1>    
-            <button onClick={clickHandler}>Click Me!</button>
+            <h1>{count}</h1>
         </>
     )
 }
