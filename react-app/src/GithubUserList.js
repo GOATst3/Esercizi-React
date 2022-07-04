ecc89cc90008dfda66b228f666dfec3c1b42bf14
Goat.St3 <stefano2301@live.it>
2022-07-04 17:51:40 +0200

    es69 done

---
+++ react-app/src/GithubUserList.js
@@ -0,0 +1,23 @@
+import {useEffect, useState } from "react"
+import GithubUser from './GithubUser'
+export default function GithubUserList(props){
+    const [users,setUsers]=useState(['GOATst3'])
+    const [newUser,setNewUser]=useState('')
+
+    function OnChangeHandler (e) {
+        setNewUser(e.target.value)
+    }
+    function Add () {
+        setUsers(oldarray=>[...oldarray,newUser])
+        setNewUser('')
+        console.log(users)
+    }
+
+    return(
+        <>
+            {users.map(item=><GithubUser username={item}/>)}
+            <input onChange={OnChangeHandler} value={newUser}></input>
+            <button onClick={Add}>add</button>
+        </>
+    )
+}
