79f0f5fa4ab292a2cfd7f7ab1d734d65a8d3070a
Goat.St3 <stefano2301@live.it>
2022-07-08 17:04:15 +0200

    es73 done

--- react-app/src/GithubUser.js
+++ react-app/src/GithubUser.js
@@ -1,16 +1,34 @@
 import {useEffect, useState } from "react"
 import UseGithubUser from "./UseGithubUser"
 export default function GithunUser(props){
-    const {data}=UseGithubUser(props.username)
+    const {data,loading,error,fetchUser}=UseGithubUser(props.username)
+    
+       useEffect(()=>fetchUser(),[])
+        
 
-    if (data?.login){
-        return(
-            <div style={{textAlign:'center'}}>
+    useEffect(()=>{
+        if(error){
+            console.error('There was an error: ',error)
+        }
+    }
+    ,[error])
+
+
+    return(
+        <div style={{textAlign:'center'}}>
+            {loading && <h1>Loading</h1>}
+            {error && <h1>{error}</h1>}
+            {data.login &&
+                <div>
                     <h1>{data.login}</h1>
                     <p>Click <a href={data.html_url}>Here</a> to go to the GitHub account</p>
-                <img src={data.avatar_url}/>
-            </div>
+                    <img src={data.avatar_url}/>
+                </div>
+            }
+        </div>
         )
-    }
-    else return <h1>{data.message}</h1>
+        
+
+
+
 }
--- react-app/src/UseGithubUser.js
+++ react-app/src/UseGithubUser.js
@@ -1,24 +1,28 @@
-import {useState, useEffect } from "react"
+import {useState} from "react"
 
 export default function UseGithubUser(username){
     const [data,setData]=useState({})  
+    const [loading,setLoading]=useState(false)  
+    const [error,setError]=useState(null)  
     
     function fetchUser (){ 
+        setLoading(true)
+
         fetch(`https://api.github.com/users/${username}`)
         .then(response=>{
             console.log('response status:',response.status)
+            if(response.status!==200) throw new Error('Connection lost or invalid Username')
             return response.json()
         })
         .then(json=>{
-            console.log(json)
             setData(json)
+            console.log(json)
         })
-        .catch(err=> console.error(err))
+        .catch(err=>{
+            setError(err.message)    
+        })
+        .finally(setLoading(false))
     }
 
-    useEffect(()=>{
-        fetchUser()
-    },[])
-
-    return {data}
+    return {data,loading,error,fetchUser}
 }
