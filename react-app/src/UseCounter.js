d61f34d13c64edb3fb6d38ad1cd7682b250534df
Goat.St3 <stefano2301@live.it>
2022-07-09 17:05:30 +0200

    es75 done

--- react-app/src/UseCounter.js
+++ react-app/src/UseCounter.js
@@ -1,12 +1,12 @@
-import {useState} from 'react'
+import {useState,useCallback} from 'react'
 
 export function useCounter(initialValue=0){
 
     const [counter,setCounter]=useState(initialValue);
 
-    const increment = () => setCounter(c => c + 1)
-    const decrement = () => setCounter(c=>c-1)
-    const reset = () => setCounter(initialValue)
+    const increment = useCallback(() => setCounter(c => c + 1),[])
+    const decrement = useCallback(() => setCounter(c=>c-1),[])
+    const reset = useCallback (() => setCounter(initialValue),[initialValue])
 
     return {counter,increment,decrement,reset}
 }
